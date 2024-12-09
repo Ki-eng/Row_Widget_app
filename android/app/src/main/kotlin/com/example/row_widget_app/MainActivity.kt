ae1977ccd051b2bf417d2836a5a6309035117af7
Kiran Saleem <saleemkiran1599@gmail.com>
2024-12-09 22:01:57 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/row_widget_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.row_widget_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Center(
+          child: Row(
+            children: [
+              Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  color: Colors.purple,
+                  child: Text(
+                    'HELLO WORLD',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )),
+              Image(
+                width: 240,
+                height: 240,
+                image: AssetImage('images/computer.jpg'),
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
